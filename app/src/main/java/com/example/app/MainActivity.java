b5326e34daea7a43d4ff62b4dc9ac8c798e03633
Kesen <kesenhoo@gmail.com>
2014-01-23 19:44:23 +0800

    commit first version code

---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity {
+
+    private static final String TAG = "MyMainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+//                Log.d(TAG, "[dispatchTouchEvent] -> ACTION_DOWN");
+//                break;
+                Log.d(TAG, "[dispatchTouchEvent] -> ACTION_DOWN, return true");
+                return true;
+            case MotionEvent.ACTION_UP:
+                Log.d(TAG, "[dispatchTouchEvent] -> ACTION_UP");
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.d(TAG, "[dispatchTouchEvent] -> ACTION_CANCEL");
+                break;
+            default:
+                break;
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d(TAG, "[onTouchEvent] -> ACTION_DOWN");
+                break;
+//            case MotionEvent.ACTION_MOVE:
+//                Log.d(TAG, "[onTouchEvent] -> ACTION_MOVE");
+//                break;
+            case MotionEvent.ACTION_UP:
+                Log.d(TAG, "[onTouchEvent] -> ACTION_UP");
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.d(TAG, "[onTouchEvent] -> ACTION_CANCEL");
+                break;
+            default:
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/app/MyButton.java
@@ -0,0 +1,63 @@
+package com.example.app;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.Button;
+
+/**
+ * Created by hook on 14-1-22.
+ */
+public class MyButton extends Button{
+
+    private static final String TAG = MyButton.class.getSimpleName();
+
+    public MyButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d(TAG, "[dispatchTouchEvent] -> ACTION_DOWN");
+                break;
+//            case MotionEvent.ACTION_MOVE:
+//                Log.d(TAG, "[dispatchTouchEvent] -> ACTION_MOVE");
+//                break;
+            case MotionEvent.ACTION_UP:
+                Log.d(TAG, "[dispatchTouchEvent] -> ACTION_UP");
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.d(TAG, "[dispatchTouchEvent] -> ACTION_CANCEL");
+                break;
+            default:
+                break;
+        }
+        return super.dispatchTouchEvent(event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d(TAG, "[onTouchEvent] -> ACTION_DOWN");
+                break;
+//            case MotionEvent.ACTION_MOVE:
+//                Log.d(TAG, "[onTouchEvent] -> ACTION_MOVE");
+//                break;
+            case MotionEvent.ACTION_UP:
+                Log.d(TAG, "[onTouchEvent] -> ACTION_UP");
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.d(TAG, "[onTouchEvent] -> ACTION_CANCEL");
+                break;
+            default:
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+
+}
